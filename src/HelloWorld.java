1256696d2fb11b6dc2385b92b2aa63f73a0abdbd
Mumtaz Ummar <g00067218@aus.edu>
2018-02-13 19:54:54 +0400

    Feature B added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,10 @@
 
-public class HelloWorld {
 
+public class HelloWorld {
+	public void addfeatureB()
+	{
+		System.out.println("Feature B added ");
+		}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
