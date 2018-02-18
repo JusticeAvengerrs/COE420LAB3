5a2b1d413d84527cdf5568f1a569ad0659f78cff
Mumtaz Ummar <g00067218@aus.edu>
2018-02-18 15:05:19 +0400

    Feature b added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,9 @@
 
 
 public class HelloWorld {
-	public void addfeatureA()
+	public void addfeatureB()
 	{
-		System.out.println("Feature A added ");
+		System.out.println("Feature B added ");
 		}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
